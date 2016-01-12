2a307c35cdd5a60aa5a92d0c8559dbbd2957ee43
danaele <dana.puechmaille@gmail.com>
2016-01-12 17:56:10 -0500

    add services

---
+++ server/plugins/static/public/services/probtrack.js
@@ -0,0 +1,21 @@
+angular.module('brainConnectivity')
+.factory('probtrack',function($q, $http, $location){
+
+	return{
+		getConnectivityDescription: function(id){
+			return $http({
+				method : 'GET',
+				url:'/probtrackConnectivityDescription'
+
+			});
+		},
+		getMatrix: function(){
+			return $http({
+			  method: 'GET',
+			  url:'/probtrackMatrix'
+			});
+		}
+			
+	};
+
+});
