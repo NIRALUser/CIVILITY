eebee2c0289c5ddabd9b17c70c2aa4fbf9325d68
danaele <danaele@email.unc.edu>
2016-02-19 16:40:40 -0500

    improve html pages

--- server/plugins/static/public/home/tracto.js
+++ server/plugins/static/public/home/tracto.js
@@ -8,21 +8,12 @@ angular.module('brainConnectivity')
   $scope.plotData = undefined;
   $scope.plotParameters = {};
 
-  $scope.submit = function(){
+  $scope.test = function(){
 
-    probtrack.getFDTMatrix()
-    .then(function(response){
-      $scope.ButtonClicked = true;
-      $scope.plotParameters.link1 = "";
-      $scope.plotParameters.link2 = "";
-      $scope.plotParameters.threshold = 0.1;
-      $scope.plotParameters.method = [true,false,false];
-      $scope.plotParameters.tension = 85;
-      $scope.plotParameters.diameter = 960
-      $scope.plotParameters.upperValue = 1;
-      $scope.plotParameters.data = response.data;
-      $scope.Plot;
-    }).catch(console.error);
+    console.log("TESSST im in scope")
+
+   var nav = document.getElementById("tracto");
+   nav.attr("color-background", "blue");
 
   }
 }]);
