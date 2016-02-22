f2f31ccf3eef3dfeff2b4a866ad2980e0a2710ef
danaele <danaele@email.unc.edu>
2016-02-22 16:24:47 -0500

    Upload file directive / service

--- server/plugins/static/public/home/home.js
+++ server/plugins/static/public/home/home.js
@@ -1,7 +1,7 @@
 
 
 angular.module('brainConnectivity')
-.controller('firstController', ['$scope','$http','probtrack', function($scope, $http, probtrack) {
+.controller('firstController', ['$scope','$http','probtrack','fileUpload', function($scope, $http, probtrack,fileUpload) {
 
   $scope.plotVisible = false ;
   $scope.ButtonClicked = false;
@@ -24,7 +24,72 @@ angular.module('brainConnectivity')
       $scope.Plot;
     }).catch(console.error);
 
+  };
+
+  $scope.paramSubmitJob = function()
+  {
+    console.log("Submit");
+    $scope.getSubjectID();
+    $scope.getLabelsetName();
+    $scope.addSurfaceColor();
+    console.log("Overlapping " + $scope.overlapping);
+    console.log("Loopcheck " + $scope.loopcheck);
+    $scope.ignoreLabel();
+    $scope.uploadFile();
+
+  };
+
+
+   $scope.ignoreLabel = function()
+  {
+    console.log("ignoreLabel : " + $scope.checkedIgnoreLabel);
+    if($scope.checkedIgnoreLabel)
+    {
+      console.log($scope.labelNameIgnore.name)
+    }
+  };
+
+
+  $scope.getSubjectID = function()
+  {
+    console.log("subject ID : " + $scope.subjectID.id);
+  };
+
+  $scope.getLabelsetName = function()
+  {
+    console.log("labelset name : "+ $scope.labelset.name);
+  };
+
+  $scope.addSurfaceColor = function()
+  {
+    console.log("two surfaces " + $scope.checkedSurfaceColored);
   }
+
+   $scope.subjectID = {
+        id: 'neonate',
+      };
+
+      $scope.labelNameIgnore = {
+        name: '0-0-0',
+      };
+
+  $scope.labelset = {
+        name: 'colour',
+      };
+
+  $scope.uploadFile = function(){
+      
+      var file = $scope.myFile1;
+               
+      console.log('file is ' );
+      console.dir(file);
+               
+      var uploadUrl = "/fileUpload";
+      fileUpload.uploadFileToUrl(file, uploadUrl);
+   };
+
+
+
 }]);
 
 
