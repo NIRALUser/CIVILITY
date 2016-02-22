fe3de8d97c2d256222a85575b63f3d3fb03b410e
danaele <danaele@email.unc.edu>
2016-02-22 17:05:23 -0500

    Start creation object job (to submit to other server)

--- server/plugins/static/public/home/home.js
+++ server/plugins/static/public/home/home.js
@@ -1,12 +1,15 @@
 
 
 angular.module('brainConnectivity')
-.controller('firstController', ['$scope','$http','probtrack','fileUpload', function($scope, $http, probtrack,fileUpload) {
+.controller('firstController', ['$scope','$http','probtrack','fileUpload', function($scope, $http, probtrack, fileUpload) {
 
   $scope.plotVisible = false ;
   $scope.ButtonClicked = false;
   $scope.plotData = undefined;
   $scope.plotParameters = {};
+
+  $scope.parametersTracto = []; 
+
 
   $scope.submit = function(){
 
@@ -33,9 +36,16 @@ angular.module('brainConnectivity')
     $scope.getLabelsetName();
     $scope.addSurfaceColor();
     console.log("Overlapping " + $scope.overlapping);
+    // $scope.parametersTracto.overlapping = $scope.overlapping;
     console.log("Loopcheck " + $scope.loopcheck);
+    //  $scope.parametersTracto.loopcheck = $scope.loopcheck;
     $scope.ignoreLabel();
     $scope.uploadFile();
+    $scope.createJobObject();
+
+
+    console.log("END ");
+    console.log($scope.parametersTracto);
 
   };
 
@@ -53,11 +63,14 @@ angular.module('brainConnectivity')
   $scope.getSubjectID = function()
   {
     console.log("subject ID : " + $scope.subjectID.id);
+     $scope.parametersTracto.push($scope.subjectID.id);
+
   };
 
   $scope.getLabelsetName = function()
   {
     console.log("labelset name : "+ $scope.labelset.name);
+    $scope.parametersTracto.push($scope.labelset.name);
   };
 
   $scope.addSurfaceColor = function()
@@ -87,6 +100,65 @@ angular.module('brainConnectivity')
       var uploadUrl = "/fileUpload";
       fileUpload.uploadFileToUrl(file, uploadUrl);
    };
+
+
+   $scope.createJobObject = function(){
+      var job = {};
+
+      job.executable = "TractograpyhWorkflow"; 
+
+      var nbParameters = 2; 
+
+
+      job.parameters = [];
+      for (var i = 0 ; i < nbParameters ; i++)
+      {
+        var param = {}; 
+        param.flag = "";
+        param.name = $scope.parametersTracto[i]
+         job.parameters.push(param);
+      }
+
+
+      job.type = "job"; 
+      job.userEmail = "danaele@email.unc.edu";
+      job.executionserver =  "testserver";
+   
+
+      console.log(job);
+      return job;
+
+
+/*    var job = {
+    "executable": "convert",
+    "parameters": [
+        {
+            "flag": "",
+            "name": "pic.jpg"
+        },
+        {
+            "flag": "",
+            "name": "pic.eps"
+        }
+    ],
+    "inputs": [
+        {
+            "name": "pic.jpg"
+        }
+    ],
+    "outputs": [
+        {
+            "type": "file",
+            "name": "pic.eps"
+        }
+    ],
+    "type": "job",
+    "userEmail": "juanprietob@gmail.com",
+    "executionserver" : "testserver"
+};*/
+
+
+   }
 
 
 
