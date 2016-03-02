411edc0215d8085980cb239e1b625bfe222b802a
danaele <danaele@email.unc.edu>
2016-03-02 13:23:04 -0500

    delete handler fucntion unused

--- server/plugins/ProbtrackConnectivity/index.js
+++ server/plugins/ProbtrackConnectivity/index.js
@@ -1,24 +1,13 @@
 
 exports.register = function (server, options, next) {
 	var handlers = require('./probtrack.handler')(server,options);
-    server.route({
-        method: 'GET',
-        path:'/probtrackMatrix',
-        handler: handlers.getMatrix
-    });
 
     server.route({
         method: 'GET',
         path:'/probtrackFDTMatrix',
         handler: handlers.getFDTMatrix
     });
-
-     server.route({
-        method: 'GET',
-        path:'/probtrackConnectivityDescription',
-        handler: handlers.getConnectivityDescription
-    });
-        
+      
     next();
 };
 
