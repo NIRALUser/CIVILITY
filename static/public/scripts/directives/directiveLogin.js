c43571eb90f0ab3ddd9322a8596ef35077a4a00a
danaele <danaele@email.unc.edu>
2016-04-28 15:56:39 -0400

    Login directive test

--- static/public/scripts/directives/directiveLogin.js
+++ static/public/scripts/directives/directiveLogin.js
@@ -1,18 +1,39 @@
 
-angular.module('brainConnectivity')
-.directive('testDir', function($routeParams,$location,clusterpost){
+angular.module('brainConnectivity', ['angular-jwt'])
+.config(function Config($httpProvider, jwtInterceptorProvider) {
+  // Please note we're annotating the function so that the $injector works when the file is minified
+  jwtInterceptorProvider.tokenGetter = ['config', function(config) {    
+  	console.log(config.url);
+  	return null;
+    return localStorage.getItem('clusterpost_token');
+  }];
+
+  $httpProvider.interceptors.push('jwtInterceptor');
+})
+.directive('createUserDir', function($routeParams,$location,clusterpost){
+
 
 function link($scope,$attrs,$filter){
+	$scope.createUser = function(){
 
+		clusterpost.createUser($scope.newUser)
+		.then(function(res){			
+			localStorage.setItem('clusterpost_token', res.token);
+		})
+		.catch(function(err){
+			console.error(err);
+		});
+
+	}	
 };
 
 return {
     restrict : 'E',
 /*    scope: {
-      testID : "="
+    	testID : "="
     },*/
     link : link,
-    templateUrl: 'views/directives/directiveTest.html'
+    templateUrl: 'views/directives/directiveLogin.html'
 }
 
 });
