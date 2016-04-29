b04fa1db6154ae52893ea04709412648caa96534
danaele <danaele@email.unc.edu>
2016-04-29 15:31:32 -0400

    Add container for login, signin, reset Password, change Password

--- static/public/scripts/directives/directiveLogin.js
+++ static/public/scripts/directives/directiveLogin.js
@@ -1,21 +1,24 @@
 
-angular.module('brainConnectivity'/*, ['angular-jwt']*/)
-/*.config(function Config($httpProvider, jwtInterceptorProvider) {
+angular.module('brainConnectivity', ['angular-jwt'])
+.config(function Config($httpProvider, jwtInterceptorProvider) {
   // Please note we're annotating the function so that the $injector works when the file is minified
   jwtInterceptorProvider.tokenGetter = ['config', function(config) {    
   	console.log(config.url);
-  	return null;
+  	//return null;
     return localStorage.getItem('clusterpost_token');
   }];
 
   $httpProvider.interceptors.push('jwtInterceptor');
-})*/
+})
 .directive('createUserDir', function($routeParams,$location,clusterpost){
+	function link($scope,$attrs,$filter){
 
+	$scope.resetPass = false;
+	$scope.signin = false;
+	$scope.login = true;
+	$scope.reset = false;
 
-function link($scope,$attrs,$filter){
 	$scope.createUser = function(){
-
 		clusterpost.createUser($scope.newUser)
 		.then(function(res){			
 			localStorage.setItem('clusterpost_token', res.token);
@@ -23,19 +26,29 @@ function link($scope,$attrs,$filter){
 		.catch(function(err){
 			console.error(err);
 		});
+	}
 
-	}	
+	$scope.resetPasswordDirective = function(){
+		$scope.resetPass = true;
+		$scope.login = false;
+	}
+
+	$scope.signinDirective = function(){
+		$scope.signin = true;
+		$scope.login = false;
+	}
 
 	$scope.login = function(){
-		clusterpost.createUser($scope.newUser)
-		.then(function(res){			
-			localStorage.setItem('clusterpost_token', res.token);
-		})
-		.catch(function(err){
-			console.error(err);
-		});
-
+		//if login : 
+		$scope.isLog = true;
 	}	
+	$scope.updatePassword = function(){
+		//Check 2 passwords are equals 
+	}	
+
+	$scope.resetPassword = function(){
+		$scope.reset = true;
+	}
 };
 
 return {
