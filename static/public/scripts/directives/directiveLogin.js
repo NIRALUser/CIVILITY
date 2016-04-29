774522eab56ed4cf2a69a7d8bf4666506f078328
danaele <danaele@email.unc.edu>
2016-04-29 11:09:34 -0400

    Create signin page and update cluster.service file with login handlers

--- static/public/scripts/directives/directiveLogin.js
+++ static/public/scripts/directives/directiveLogin.js
@@ -1,6 +1,6 @@
 
-angular.module('brainConnectivity', ['angular-jwt'])
-.config(function Config($httpProvider, jwtInterceptorProvider) {
+angular.module('brainConnectivity'/*, ['angular-jwt']*/)
+/*.config(function Config($httpProvider, jwtInterceptorProvider) {
   // Please note we're annotating the function so that the $injector works when the file is minified
   jwtInterceptorProvider.tokenGetter = ['config', function(config) {    
   	console.log(config.url);
@@ -9,13 +9,24 @@ angular.module('brainConnectivity', ['angular-jwt'])
   }];
 
   $httpProvider.interceptors.push('jwtInterceptor');
-})
+})*/
 .directive('createUserDir', function($routeParams,$location,clusterpost){
 
 
 function link($scope,$attrs,$filter){
 	$scope.createUser = function(){
 
+		clusterpost.createUser($scope.newUser)
+		.then(function(res){			
+			localStorage.setItem('clusterpost_token', res.token);
+		})
+		.catch(function(err){
+			console.error(err);
+		});
+
+	}	
+
+	$scope.login = function(){
 		clusterpost.createUser($scope.newUser)
 		.then(function(res){			
 			localStorage.setItem('clusterpost_token', res.token);
