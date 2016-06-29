6f70501f7907a0e6afdcfb0ae70522c8001e6971
danaele <danaele@email.unc.edu>
2016-06-29 11:46:05 -0400

    ENH : new architecture and add CMakeLists.txt to build tools ExtractLabelSurfaces and DWIConvert

---
+++ src/civility-static/index.js
@@ -0,0 +1,27 @@
+exports.register = function (server, options, next) {
+	server.path(__dirname);
+	
+	server.route({
+		path: '/',
+		method: '*',
+		handler: function (request, reply) {
+			reply.redirect('/civility/');
+		}
+	});
+	
+	server.route({
+		path: '/civility/{path*}',
+		method: 'GET',
+		config: {
+			handler: {
+				directory: { path: './civility', listing: false, index: true }
+			},
+			description: 'This route serves the static website of CIVILITY. Everything inside the plugins src/CIVILITY-PUBLIC/ directory will be directly accessible under this route.'
+		}
+	});
+    next();
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
