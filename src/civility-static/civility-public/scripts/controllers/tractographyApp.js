db5bc6e615ce20e7ea4fa0a07e27b41c79dba184
juanprietob <juanprietob@gmail.com>
2016-10-25 17:57:07 -0400

    ENH: Add download callback

--- src/civility-static/civility-public/scripts/controllers/tractographyApp.js
+++ src/civility-static/civility-public/scripts/controllers/tractographyApp.js
@@ -7,5 +7,29 @@ angular.module('CIVILITY')
 		$scope.activeTab = 3;
 	}
 
+	$scope.downloadCallback = function(job){
+
+		var output = _.find(job.outputs, function(output){
+			return output.type === "tar.gz";
+		});
+
+		if(output){
+			clusterpostService.getDownloadAttachmentURL(job._id, output.name)
+			.then(function(res){
+				var pom = document.createElement('a');
+				pom.setAttribute('href', res);
+				pom.setAttribute('download', output.name + ".tar.gz");
+
+				pom.dataset.downloadurl = ['application/octet-stream', pom.download, pom.href].join(':');
+				pom.draggable = true; 
+				pom.classList.add('dragout');
+
+				pom.click();
+
+			})
+			.catch(console.error);
+		}
+	}
+
 });
 
