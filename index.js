34dcba85cd18ef3602479a9c8b4b1043287f48a8
juanprietob <juanprietob@gmail.com>
2017-03-09 14:14:58 -0500

    BUG: Modify server startup mechanism

--- index.js
+++ index.js
@@ -82,25 +82,4 @@ const startServer = function(cluster){
     });
 }
 
-if(env === 'production'){
-    const cluster = require('cluster');
-    const numCPUs = require('os').cpus().length;
-
-    if (cluster.isMaster) {
-      // Fork workers.
-      for (var i = 0; i < numCPUs; i++) {
-        cluster.fork();
-      }
-
-      cluster.on('exit', (worker, code, signal) => {
-        console.log("worker ", worker.process.pid,"died");
-      });
-      
-    } else {
-        startServer(cluster);
-    }
-}else{
-
-    startServer();
-}
-
+startServer();
