c84652c4e57266f80af6f98f503281f3d46919f0
Juan Carlos Prieto <juanprietob@gmail.com>
2018-11-09 11:53:43 -0500

    ENH: Print messages while submitting job via util

--- src/civility-util/index.js
+++ src/civility-util/index.js
@@ -127,19 +127,19 @@ clusterpost.start(path.join(os.homedir(), '.civility.json'))
                 "outputs": [
                     {
                         "type": "file",
+                        "name": civility_params['name'] + "/Network_overlapping_loopcheck/fdt_network_matrix"  
+                    },
+                    {
+                        "type": "file",
+                        "name": civility_params['name'] + "/" + civility_params['table']
+                    },
+                    {
+                        "type": "file",
                         "name": "stdout.out"
                     },
                     {
                         "type": "file",
                         "name": "stderr.err"
-                    },
-                    {
-                        "type": "file",
-                        "name": civility_params['name'] + "/Network_overlapping_loopcheck/fdt_network_matrix"  
-                    },
-                    {
-                        "type": "file",
-                        "name": civility_params['name'] + "/" + civility_params['table'];
                     }
                 ],
                 "jobparameters" : [
@@ -205,7 +205,7 @@ clusterpost.start(path.join(os.homedir(), '.civility.json'))
 
             job.executionserver = argv["executionserver"]? argv["executionserver"] : "killdevil";
 
-            console.log("Submitting job:" job);
+            console.log("Submitting job:", job);
 
             return clusterpost.createAndSubmitJob(job, inputfiles);
         }, {concurrency: 1});
